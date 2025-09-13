380666373e3143c018b37bad18154a7364b26777
Karabo Mogano <karabomogano27@gmail.com>
2025-09-13 23:36:00 +0200

    Improved error handling with response.ok check using if-stametent

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ async function fetchData() {
 
     try {
         const response = await fetch(API_ENDPOINT);
+        if(!response.ok){
+            throw new Error(`HTTP error!status:${response.status}`);
+        }
+
         const data = await response.json();
         dataContainer.innerHTML = `<strong>Data Loaded:</strong> ${data.message}`;
+    
     } catch (error) {
-        
+        dataContainer.innerHTML = `<span class="error">Failed to load data.</span>`;
+        console.error('Fetch Error:', error);   
     }
     
 }
